527e7bf519916e10b30641c2b194acd8cbedb91b
jeslbro <jess.brown@rxa.io>
2022-12-16 10:59:48 -0800

    updating reqs

--- readysignal/__init__.py
+++ readysignal/__init__.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import time
+import json
 
 
 def connect_to_readysignal(access_token, signal_id=None, output=False):
@@ -11,7 +12,7 @@ def connect_to_readysignal(access_token, signal_id=None, output=False):
     :param signal_id: signal's unique ID number, to show signal details
     or output full signal
     :param output: show signal data or not
-    :return:
+    :return: request response as json
     """
     try:
         # show signal
@@ -115,3 +116,56 @@ def signal_to_csv(access_token, signal_id, file_name):
 
     output = get_signal_pandas(access_token, signal_id)
     output.to_csv(file_name, index=False)
+
+
+def delete_signal(access_token, signal_id):
+    """
+    USE WITH CAUTION. deletes a signal from the Ready Signal platform
+    :param access_token: user's unique access token
+    :param signal_id: signal's unique ID number
+    :return: requests response object
+    """
+    url = f'http://app.readysignal.com/api/signals/{str(signal_id)}'
+
+    headers = {'Authorization': 'Bearer ' + str(access_token),
+               'Accept': 'application/json'}
+    req = requests.delete(url, headers=headers)
+    print(req.json())
+    return req
+
+
+def auto_discover(access_token, geo_grain, filename=None, df=None):
+    """
+    Creates a signal using your own data and the Auto Discover feature. Please check Ready Signal site for tips on
+    how to format your data. Currently only available at the "State" or "Country" geo grain.
+    :param access_token: user's unique access token
+    :param geo_grain: geographic grain of data upload: "State" or "Country"
+    :param filename: if using file upload, filename. Accepted file formats: .CSV or .XLSX. Column naming schema should
+    be "Date" (YYYY-MM-DD), "State" (MI) if geo_grain="State", "Value" (int or float, no strings).
+    Not to be used with 'df'
+    :param df: if using Pandas DataFrame upload, DataFrame object. Column naming schema should
+    be "Date" (YYYY-MM-DD), "State" (MI) if geo_grain="State", "Value" (int or float, no strings).
+    Not to be used with 'filename'
+    :return: requests response object
+    """
+    base_url = 'https://app.readysignal.com/api/auto-discovery'
+
+    if geo_grain not in ['State', 'Country']:
+        exit('Geographic grain for data must be "State" or "Country"')
+    elif filename and df is not None:
+        exit('Please use only one of "filename" or "df" for Auto Discover feature')
+
+    elif filename:
+        url = base_url + '/file'
+        req = requests.post(url, data={'geo_grain': geo_grain}, files={'file': open(filename, 'rb')},
+                            headers={'Authorization': 'Bearer ' + str(access_token)})
+    elif df is not None:
+        url = base_url + '/array'
+        body = {"geo_grain": geo_grain, "data": df.to_dict(orient='records')}
+
+        req = requests.post(url, json=body, headers={'Authorization': 'Bearer ' + str(access_token)})
+
+    else:
+        exit('Missing data source, please provide "filename" as filepath or "df" as Pandas dataframe')
+    print(req.json())
+    return req
