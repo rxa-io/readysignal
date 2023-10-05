cdb475ab1f454d607896b719fd20793ee68b5b77
Jessica Parks <jparks@onemagnify.com>
2023-10-05 15:54:32 -0400

    allow multiple features for feature_info

--- readysignal/__init__.py
+++ readysignal/__init__.py
@@ -222,8 +222,15 @@ def connect_to_readysignal_features(access_token, features = None, start_date = 
             url = f'https://staging.app.readysignal.com/api/bank-of-mexico/feature/{features[0]}/details'
 
         # show feature's information
-        elif features and len(features) == 1:
-            url = f'https://staging.app.readysignal.com/api/bank-of-mexico/feature/{features[0]}'
+        elif features:
+            feat_info = {}
+            for i in range(len(features)):
+                url = f'https://staging.app.readysignal.com/api/bank-of-mexico/feature/{features[i]}'
+                headers = {'Authorization': 'Bearer ' + str(access_token),
+                        'Accept': 'application/json'}
+                req = requests.get(url, headers=headers)
+                feat_info[features[i]] = list(req.json().values())[0]
+            return feat_info
 
         # list all Bank of Mexico features
         else:
